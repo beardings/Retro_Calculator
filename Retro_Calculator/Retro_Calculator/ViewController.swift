49404c199b7ccc5937a5c5425cbbe17daf5c0ca0
Nikolas Ponomarov <beardings@users.noreply.github.com>
2017-08-24 00:37:26 +0300

    add image and using UIStackView for setting view

---
+++ Retro_Calculator/Retro_Calculator/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Retro_Calculator
+//
+//  Created by Nikolas Ponomarov on 23.08.17.
+//  Copyright © 2017 Nikolas Ponomarov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
