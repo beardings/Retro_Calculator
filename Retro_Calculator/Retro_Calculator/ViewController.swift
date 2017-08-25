58a700bd6376fa1461f621c17e61e7789d9f7e24
Nikolas Ponomarov <beardings@users.noreply.github.com>
2017-08-25 23:40:28 +0300

    custom and set music for button

--- Retro_Calculator/Retro_Calculator/ViewController.swift
+++ Retro_Calculator/Retro_Calculator/ViewController.swift
@@ -6,13 +6,41 @@
 //  Copyright © 2017 Nikolas Ponomarov. All rights reserved.
 //
 
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
 
+    var btnSound: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // указываем путь к файлу
+        let path = Bundle.main.path(forResource: "btn", ofType: "wav")
+        let soundURL = URL(fileURLWithPath: path!)
+        
+        // страхуем себя на случай ошибки
+        do {
+            btnSound = try AVAudioPlayer(contentsOf: soundURL)
+            btnSound.play()
+        }
+        catch let err as NSError {
+            print(err.localizedDescription)
+        }
+    }
+    
+    // линк всех кнопок
+    @IBAction func numberPressed(sender: UIButton) {
+        playSound()
     }
 
+    // функция для проигрывания кнопки
+    func playSound () {
+        if btnSound.isPlaying {
+            btnSound.stop()
+        }
+        btnSound.play()
+    }
 }
 
